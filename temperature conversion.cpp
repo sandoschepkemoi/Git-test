95e515fc2a1443e5ee2242d9f1f3aa3786ca375d
sandos chepkemoi kipkosgey <73238300+sandoschepkemoi@users.noreply.github.com>
2025-09-22 15:25:50 +0300

    Update temperature conversion.cpp

--- temperature conversion.cpp
+++ temperature conversion.cpp
@@ -1,1 +1,13 @@
+/*write a program to prompt user to enter the temperature in farenheight and convert it to celcius*/
+# include <iostream>
 
+using namespace std;
+
+int main(){
+    double farenheight, celcius;
+    cout<<"Enter temperature in farenheight";
+    cin>>farenheight;
+    celcius = (farenheight - 32) *5/9;
+    cout<< celcius;
+    return 0;
+}
